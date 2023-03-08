aeac716da453b337acbee886eb3b197436db1ae9
Olga <olga.sleepless@gmail.com>
2023-03-08 08:11:50 -0500

    W9D3

--- W9D3/practice-for-ch-js-first-ajax-main/index.js
+++ W9D3/practice-for-ch-js-first-ajax-main/index.js
@@ -1,3 +1,19 @@
 console.log("Hello from the console!");
 
 // Your code here
+fetch('https://api.openweathermap.org/data/2.5/weather?q=London,UK&appid=bcb83c4b54aee8418983c2aff3073b3b')
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Response not OK');
+    }
+  })
+  .then(data => {
+    console.log('Success!', data);
+  })
+  .catch(error => {
+    console.log('Failure!', error);
+  });
+  console.log("The AJAX request has been dispatched.");
+
